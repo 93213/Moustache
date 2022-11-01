b064ba56c701b150a0cd8bc4eea0f95163b17692
93213 <102404282+93213@users.noreply.github.com>
2022-11-01 16:58:59 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+noseX=0;
+noseY=0;
+
 function preload() {
+    moustache = loadImage('https://i.postimg.cc/LXFj2ZHt/moustache-473661-1280.png')
 }
 
 function setup() {
@@ -18,6 +22,7 @@ function modelLoaded() {
 
 function draw() {
     image(video, 0, 0, 300, 300);
+    image(moustache, noseX, noseY, 60, 30);
 }
 
 function take_snapshot(){
@@ -29,7 +34,9 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
-        console.log("nose x = " + results[0].pose.net.x);
-        console.log("nose y = " + results[0].pose.net.y);
+        noseX = results[0].pose.nose.x-25;
+        noseY = results[0].pose.nose.y+5.5;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
     }
 }
